095a90ed656cb163f8e17e36feedbda0b6786ade
Jarzon <j@masterj.net>
2017-05-10 03:36:03 -0400

    Move assets in pack Refactor the assets config file Reload main asset config on assets configs change Remove recursive glob from assets config and gulpfile watch Add main asset config file in gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ var concat = require('gulp-concat');
 var rename = require("gulp-rename");
 var merge = require('gulp-merge-json');
 var del = require('del');
-var config = require('./app/config/assets.json');
+var fs = require('fs');
+
+try {
+    var config = require('./app/config/assets.json');
+} catch(error) {
+    var config = {};
+}
 
 gulp.task('js-clean', function () {
     return del(['public/js/*']);
@@ -22,31 +28,52 @@ gulp.task('msg-clean', function () {
     return del(['app/config/messages.json']);
 });
 
-gulp.task('js-build', function() {
-    var tasks = config.js.files.map(function(file) {
-        return gulp.src(file[0])
-            .pipe(concat(file[1]))
+gulp.task('assets-clean', function () {
+    return del(['app/config/assets.json']);
+});
+
+gulp.task('assets-reload', ['assets-build'], function(cb) {
+    return setTimeout(() => {
+        fs.readFile('./app/config/assets.json', 'utf8', function (err, data) {
+            if (err) cb(err);
+            config = JSON.parse(data);
+            cb();
+        });
+    }, 50);
+
+});
+
+gulp.task('rebuild-all', ['assets-reload'], function() {
+    gulp.start('js-build');
+    gulp.start('css-build');
+    gulp.start('img-build');
+});
+
+gulp.task('js-build', ['js-clean'], function() {
+    var tasks = Object.keys(config.js.files).map(function(key) {
+        return gulp.src(config.js.files[key])
+            .pipe(concat(key))
             .pipe(gulp.dest(config.js.destination));
     });
 
     return es.concat.apply(null, tasks);
 });
 
-gulp.task('css-build', function() {
-    var tasks = config.css.files.map(function(file) {
-        return gulp.src(file[0])
-            .pipe(concat(file[1]))
+gulp.task('css-build', ['css-clean'], function() {
+    var tasks = Object.keys(config.css.files).map(function(key) {
+        return gulp.src(config.css.files[key])
+            .pipe(concat(key))
             .pipe(gulp.dest(config.css.destination));
     });
 
     return es.concat.apply(null, tasks);
 });
 
-gulp.task('img-build', function() {
-    var tasks = config.img.files.map(function(file) {
-        return gulp.src(file[0])
+gulp.task('img-build', ['img-clean'], function() {
+    var tasks = Object.keys(config.img.files).map(function(key) {
+        return gulp.src(config.img.files[key])
             .pipe(rename(function (path) {
-                path.dirname = file[1];
+                path.dirname = key;
             }))
             .pipe(gulp.dest(config.img.destination));
     });
@@ -55,16 +82,23 @@ gulp.task('img-build', function() {
 });
 
 gulp.task('msg-build', function() {
-    gulp.src('src/**/config/messages.json')
+    gulp.src('src/*/config/messages.json')
         .pipe(merge({fileName: 'messages.json', jsonSpace: ''}))
         .pipe(gulp.dest('app/config/'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/**/assets/js/*.js', ['js-clean', 'js-build']);
-    gulp.watch('src/**/assets/css/*.css', ['css-clean', 'css-build']);
-    gulp.watch('src/**/assets/img/*', ['img-clean', 'img-build']);
-    gulp.watch('src/**/config/messages.json', ['msg-clean', 'msg-build']);
+gulp.task('assets-build', ['assets-clean'], function() {
+    gulp.src('src/*/config/assets.json')
+        .pipe(merge({fileName: 'assets.json', jsonSpace: '', concatArrays: true}))
+        .pipe(gulp.dest('app/config/'));
 });
 
-gulp.task('default', ['watch'], function(){});
+gulp.task('watch', function() {
+    gulp.watch('src/*/assets/js/*.js', ['js-build']);
+    gulp.watch('src/*/assets/css/*.css', ['css-build']);
+    gulp.watch('src/*/assets/img/*', ['img-build']);
+    gulp.watch('src/*/config/messages.json', ['msg-clean', 'msg-build']);
+    gulp.watch('src/*/config/assets.json', ['rebuild-all']);
+});
+
+gulp.task('default', ['rebuild-all', 'watch'], function(){});
